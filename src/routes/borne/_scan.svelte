4ad60b13f1d1080bebffec382964331171ac4e82
Team Timefast <direction@timefast.fr>
2021-08-15 04:17:35 +0200

    Dirty PoC : history, and starts parsing with CRLF instead of timeout.

--- src/routes/borne/_scan.svelte
+++ src/routes/borne/_scan.svelte
@@ -11,21 +11,43 @@
 	let code: string = '';
 	let codeFoundPromise: Promise<CommonCertificateInfo> | undefined = undefined;
 
-	let timeout: NodeJS.Timeout | undefined = undefined;
+	//let timeout: NodeJS.Timeout | undefined = undefined;
 	let reset_timeout: NodeJS.Timeout | undefined = undefined;
+	let scanning = false;
+
+	interface PassHistory {
+		first_name: string;
+		last_name: string;
+		date_of_birth: Date;
+		validated: bool;
+	}
+
+	type PassHistoryEntry = PassHistory;
 
 	let last_event: KeyboardEvent | null = null;
 	let validated_passes: Map<string, number> = new Map();
+	let passes_history: PassHistoryEntry[] = [];
 
 	const prevent_revalidation_before_ms = (prevent_revalidation_before_minutes || 0) * 60 * 1000;
 
 	function onKeyPress(event: KeyboardEvent) {
 		last_event = event;
-		if (event.key.length > 1) return;
+		if (event.key.length > 1) {
+			launchParsing(code);
+			//code = '';
+			return;
+		}
+		if(scanning == false) {
+			scanning = true;
+			code = '';
+			if(codeFoundPromise) {
+				codeFoundPromise = undefined;
+			}
+		}
 		code += event.key;
-		if (timeout !== undefined) clearTimeout(timeout);
+		//if (timeout !== undefined) clearTimeout(timeout);
 		if (reset_timeout !== undefined) clearTimeout(reset_timeout);
-		timeout = setTimeout(launchParsing, decode_after_s * 1000, code);
+		//timeout = setTimeout(launchParsing, decode_after_s * 1000, code);
 		event.preventDefault();
 	}
 
@@ -37,17 +59,21 @@
 	async function validateCertificateCode(code: string): Promise<CommonCertificateInfo> {
 		const cert = await parse_any(code);
 		const error = findCertificateError(cert);
-		if (error) throw new Error(error);
+
+		if (error) {
+                        return {cert, error: new Error(error)};
+		}
 		const last_validated = validated_passes.get(code);
+
 		if (last_validated && last_validated > Date.now() - prevent_revalidation_before_ms) {
 			const duration_minutes = ((Date.now() - last_validated) / 60 / 1000) | 0;
-			throw new Error(
-				`Passe déjà scanné par quelqu'un d'autre il y a ` +
-					(duration_minutes ? duration_minutes + ' minutes.' : "moins d'une minute.")
-			);
+                        return {cert, error: new Error(
+                                `Passe déjà scanné par quelqu'un d'autre il y a ` +
+                                        (duration_minutes ? duration_minutes + ' minutes.' : "moins d'une minute.")
+                        )};
 		}
 		validated_passes.set(code, Date.now());
-		return cert;
+		return {cert};
 	}
 
 	async function makeRequest(r: HTTPRequest) {
@@ -68,20 +94,38 @@
 		if (codeFoundPromise) return;
 		console.log('Detected code before reset: ', code_input);
 		codeFoundPromise = validateCertificateCode(code_input);
-		codeFoundPromise.then(onValid, onInvalid);
-		timeout = undefined;
+		codeFoundPromise.then(({error, cert}) => {
+			let data: PassHistoryEntry = {
+				first_name: cert.first_name,
+				last_name: cert.last_name,
+				date_of_birth: cert.date_of_birth,
+				validated: false
+			};
+			if(error == undefined) {
+				data = Object.assign(data, {validated: true});
+				onValid();
+			} else {
+				data = Object.assign(data, {validated: false});
+				onInvalid();
+			}
+			reset_timeout = setTimeout(() => {
+				codeFoundPromise = undefined;
+				passes_history = [data, ...passes_history].slice(0, 4);
+			}, reset_after_s * 1000);
+		});
+		//timeout = undefined;
 		code = '';
-		reset_timeout = setTimeout(() => {
-			codeFoundPromise = undefined;
-		}, reset_after_s * 1000);
+		scanning = false;
 	}
 
-	function showName({ first_name, last_name }: CommonCertificateInfo): string {
+	function showName({ first_name, last_name, date_of_birth }: CommonCertificateInfo): string {
 		return (
 			(first_name[0] || '').toUpperCase() +
 			first_name.slice(1).toLowerCase() +
 			' ' +
-			last_name.toUpperCase()
+			last_name.toUpperCase() +
+			' ' +
+			date_of_birth.toLocaleDateString()
 		);
 	}
 </script>
@@ -92,19 +136,21 @@
 	class="main container"
 	style="font-family: {config.font || 'inherit'}; font-size: {config.font_size || 16}px"
 >
-	{#if timeout !== undefined}
+
+	{#if scanning !== false}
 		Scan du QR code en cours...
 	{:else if codeFoundPromise != undefined}
 		{#await codeFoundPromise}
 			Décodage du code...
-		{:then pass}
+		{:then passOrErr}
+                    {#if passOrErr.error == undefined}
 			<!-- svelte-ignore a11y-media-has-caption -->
 			<audio autoplay src="{assets}/valid.mp3" />
 			<div class="alert alert-success" role="alert">
 				<div class="row">
 					<div class="col-md-2"><div class="sign shallpass" /></div>
 					<div class="col-md-10">
-						<h3>Bienvenue, {showName(pass)}</h3>
+						<h3>Bienvenue, {showName(passOrErr.cert)}</h3>
 						<p>Votre passe est validé.</p>
 						<div class="progress">
 							<div
@@ -116,7 +162,7 @@
 					</div>
 				</div>
 			</div>
-		{:catch err}
+		    {:else}
 			<!-- svelte-ignore a11y-media-has-caption -->
 			<audio autoplay src="{assets}/invalid.mp3" />
 			<div class="alert alert-danger" role="alert">
@@ -124,7 +170,7 @@
 					<div class="col-md-2"><div class="sign shallnotpass" /></div>
 					<div class="col-md-10">
 						<h3>Passe sanitaire invalide</h3>
-						<p class="font-monospace">{err.message}</p>
+						<p class="font-monospace">{passOrErr.error.message}</p>
 						<div class="progress">
 							<div
 								class="progress-bar bg-danger animate"
@@ -135,6 +181,7 @@
 					</div>
 				</div>
 			</div>
+                    {/if}
 		{/await}
 	{:else}
 		<div class="row justify-content-center w-100">
@@ -146,6 +193,29 @@
 		<h1>{config.title}</h1>
 		<p>{config.description}</p>
 	{/if}
+
+        {#each passes_history as passHistoryEntry}
+            {#if passHistoryEntry.validated == false}
+                        <div class="alert alert-danger" role="alert">
+                                <div class="row">
+                                        <div class="col-md-2"><div class="sign shallnotpass" /></div>
+                                        <div class="col-md-10">
+                                                <h3>{showName(passHistoryEntry)}</h3>
+                                        </div>
+                                </div>
+                        </div>
+             {:else}
+                        <div class="alert alert-success" role="alert">
+                                <div class="row">
+                                        <div class="col-md-2"><div class="sign shallpass" /></div>
+                                        <div class="col-md-10">
+                                                <h3>{showName(passHistoryEntry)}</h3>
+                                        </div>
+                                </div>
+                        </div>
+             {/if}
+        {/each}
+
 
 	{#if config.video_scan}
 		<div class="videoinput w-100" style="display: {codeFoundPromise ? 'none' : 'flex'}">
@@ -162,6 +232,8 @@
 			<p class="text-break font-monospace">{code}</p>
 			<p>Code length: {code.length}</p>
 			<p>Last key pressed: {JSON.stringify(last_event?.key, null, ' ')}</p>
+			<p>Scanning: {scanning ? 'yes' : 'no'}</p>
+			<p>Historique : {JSON.stringify(passes_history)}</p>
 		</div>
 	{/if}
 
